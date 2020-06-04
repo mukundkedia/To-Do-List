75e23675ba2af8ffe459a716978f0f426e66df50
Mukund Kedia <mukundkedia7478@pop-os.localdomain>
2020-06-04 16:48:13 +0530

    Updates

---
+++ precache-manifest.2b5dc20310b23dd4944426b8ef8139df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2ac1c15da5181bf4a60b3706e83feb",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "35321d68520e3e6bdbe9",
+    "url": "/To-Do-List/static/css/main.9134bf1e.chunk.css"
+  },
+  {
+    "revision": "ad2e9a83feaed00fe4b8",
+    "url": "/To-Do-List/static/js/2.69c01b30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.69c01b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35321d68520e3e6bdbe9",
+    "url": "/To-Do-List/static/js/main.f106dfda.chunk.js"
+  },
+  {
+    "revision": "67ebe2d5282457ba6cf0",
+    "url": "/To-Do-List/static/js/runtime-main.6e151a92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
